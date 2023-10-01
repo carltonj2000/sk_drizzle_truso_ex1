b31accf2a5d60f344f09e8f5dc0f5c948e4f437b
Carlton Joseph <carlton.joseph@gmail.com>
2023-10-01 15:58:13 -0700

    add crud actions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+import LoaderIcon from './loader-icon.svelte';
+import PlusIcon from './plus-icon.svelte';
+
+export { LoaderIcon, PlusIcon };
